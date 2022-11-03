0109aac163f34db508e4c0e172fa3b07790ec47d
Sacramentix <benji.u1225u@gmail.com>
2022-11-03 17:26:13 +0100

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import vue from '@astrojs/vue';
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    preact(), 
-    vue(),
     sitemap()
   ],
   vite: {
