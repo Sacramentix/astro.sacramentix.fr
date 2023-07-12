21f0bccd05fd6819448634f1a4ad340b9addcadb
Sacramentix <benji.u1225u@gmail.com>
2023-07-12 20:16:50 +0200

    add ressources page where i share youtube channel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
+import Icons from 'unplugin-icons/vite';
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sitemap()
-  ],
+  integrations: [sitemap(), vue()],
   vite: {
     ssr: {
-      external: ["svgo"],
+      external: ["svgo"]
     },
-  },
+    plugins: [
+      Icons({
+        compiler: 'astro'
+      })
+    ]
+  }
 });
